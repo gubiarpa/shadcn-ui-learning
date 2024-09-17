a656f166ed8d2c124f6ea9ca015a3f7385ed2134
Billy Arredondo <billyarredondo.coder@gmail.com>
2024-09-16 23:39:26 -0500

    Set Pages
    Even including a layout in the url dashboard

---
+++ src/app/dashboard/accordion/page.tsx
@@ -0,0 +1,7 @@
+export default function AccordionPage() {
+	return (
+		<>
+			<h1>Accordion Page!</h1>
+		</>
+	)
+}
---
+++ src/app/dashboard/alert/page.tsx
@@ -0,0 +1,7 @@
+export default function AlertPage() {
+	return (
+		<>
+			<h1>Alert Page!</h1>
+		</>
+	)
+}
